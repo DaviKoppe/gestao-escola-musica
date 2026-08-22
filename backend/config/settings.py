6aa2ca80bebbb499a051c5bdbb967d1f565d5d5d
Dxetreme <davikoppemm@gmail.com>
2026-08-22 19:55:52 -0300

    Configura domínio próprio de produção

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -29,10 +29,10 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = [
-    "localhost",
-    "127.0.0.1",
-]
+ALLOWED_HOSTS = os.getenv(
+    "ALLOWED_HOSTS",
+    "localhost,127.0.0.1,gestao-escola-musica.onrender.com",
+).split(",")
 
 
 # Application definition
@@ -61,10 +61,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-]
+CORS_ALLOWED_ORIGINS = os.getenv(
+    "CORS_ALLOWED_ORIGINS",
+    ""
+).split(",")
 
 ROOT_URLCONF = 'config.urls'
 
